1e502abd13168834b4fb66727416f32fc926c5cc
Kelsy McColgan <kelsy@ucrwpa-1-1-10-25-2-153.wnet.ucr.edu>
2018-11-11 15:37:46 -0800

    Initial Commit

---
+++ Dog3.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog3.0
+//
+//  Created by Kelsy McColgan on 11/11/18.
+//  Copyright © 2018 Kelsy McColgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
